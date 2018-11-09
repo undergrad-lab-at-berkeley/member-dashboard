aecbf8ff5b6e2f5ae4d06b49cdd1565fff91d08b
Neil Toledo <ntoledo@berkeley.edu>
2018-11-08 21:51:56 -0800

    defined all initial models

--- ulabDashboard/dashboard/admin.py
+++ ulabDashboard/dashboard/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-# from .models import Question
+from .models import Member, Department, Lab, Announcement
 
-# admin.site.register(Question)
+admin.site.register(Member)
+admin.site.register(Department)
+admin.site.register(Lab)
+admin.site.register(Announcement)
--- ulabDashboard/dashboard/models.py
+++ ulabDashboard/dashboard/models.py
@@ -6,8 +6,45 @@ from django.db import models
 #     pub_date = models.DateTimeField('date published')
 
 class Member(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
     email = models.EmailField()
-    join_date = models.DateTimeField('date published')
+    join_date = models.DateTimeField('Date Joined')
     active = models.BooleanField()
-#     question_text = models.CharField(max_length=200)
+    bio = models.TextField(max_length=300)
+    # linkedin = models.URLField(null=True)
+    # github = models.URLField(null=True)
+    # website = models.URLField(null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    executives = models.ManyToManyField(Member, related_name='executives')
+    members = models.ManyToManyField(Member, related_name='members')
+
+    def __str__(self):
+        return self.name
+
+class Lab(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
+    description = models.TextField(max_length=1000, null=True)
+    start_date = models.DateTimeField('Date Started', null=True)
+    end_date = models.DateTimeField('Date Completed', null=True)
+    manager = models.ManyToManyField(Member, related_name='managers')
+    researchers = models.ManyToManyField(Member, related_name='researchers')
+    # blog_post = models.URLField(initial='https://medium.com/undergraduate-laboratory-berkeley')
+
+    def __str__(self):
+        return self.name
+
+class Announcement(models.Model):
+    author = models.ForeignKey(Member, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    bio = models.TextField(max_length=750)
+    date_posted = models.DateTimeField('Date Posted')
+    tags = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.title
