e06003de8bc2068b3f3d81f7fcc05bda4d5341b5
Neil Toledo <ntoledo@berkeley.edu>
2018-11-23 00:27:44 -0800

    resolving git errors

--- ulabDashboard/dashboard/views.py
+++ ulabDashboard/dashboard/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Announcement, Member, Project
 from django.contrib.auth.forms import AuthenticationForm
-# form = AuthenticationForm()
+from django.db.models import Value
+from django.db.models.functions import Concat
+
+
+import pdb
 
 
 # Create your views here.
@@ -32,8 +36,17 @@ def profile(request, username):
     return render(request, 'dashboard/profile.html', context)
 
 def people_directory(request):
+    search_val = request.GET.get('search', '')
+
+    # Filter with Full Name
+    users = User.objects.annotate(
+      full_name=Concat('first_name', Value(' '), 'last_name'),
+    ).filter(full_name__icontains=search_val)
+    member_ids = users.values_list('member', flat=True)
+    people = Member.objects.filter(id__in=member_ids)
+
     context = {
-      'people': Member.objects.all(),
+      'people': people,
       'form': AuthenticationForm()
     }
     return render(request, 'dashboard/people_directory.html', context)
@@ -47,8 +60,15 @@ def project(request, proj_id):
     return render(request, 'dashboard/project.html', context)
 
 def projects_directory(request):
+    search_val = request.GET.get('search', '')
+
+    projects_by_name = Project.objects.filter(name__icontains=search_val)
+    projects_by_desc = Project.objects.filter(description__icontains=search_val)
+    projects = (projects_by_name | projects_by_desc).distinct()
+
     context = {
-      'projects': Project.objects.all(),
+      # 'projects': Project.objects.all(),
+      'projects': projects,
       'form': AuthenticationForm()
     }
     return render(request, 'dashboard/projects_directory.html', context)
