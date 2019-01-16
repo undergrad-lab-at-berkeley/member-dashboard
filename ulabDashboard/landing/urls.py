45f2f9fae55d2b3f947edefd0e784ed6cea69ef2
Neil Toledo <ntoledo@berkeley.edu>
2019-01-16 03:06:18 -0800

    added lab pages w/ content; lab execs not implemented yet

--- ulabDashboard/landing/urls.py
+++ ulabDashboard/landing/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
+    path('lab/<slug:lab>/', views.lab, name='lab'),
     # path('', include('django.contrib.auth.urls')),
 ]
